24c651cdd79010773933d78f8084dd838624b0a8
Marius Storhaug <marstor@hotmail.com>
2025-02-13 10:48:43 +0100

    Add initial test configurations, modules, and functions (#13)
    ## Description
    
    This pull request updates the GitHub Actions workflows, the addition of
    new helper scripts, and changes to the action configuration.
    
    ### Updates to GitHub Actions workflows
    
    
    *
    [`.github/workflows/Action-Test.yml`](diffhunk://#diff-a12ae5c885b0673c0ff6f70c2670886907590d624626e07da4c52e01aeaf56a4R11-R45):
    Added environment variables, updated permissions, and modified job steps
    to include initializing the environment and uploading module artifacts.
    *
    [`.github/workflows/Auto-Configure.yml`](diffhunk://#diff-562d3c45cd0b18dab88b67d776b74c8115b8775d4ffffacf2b064a23469f5443L1-L34):
    Removed the Auto-Configure workflow.
    *
    [`.github/workflows/Auto-Document.yml`](diffhunk://#diff-d4d2f065436a753ebb20ab1c11aaf55c1ae9fbad1f8c4a76526f16a451759633L1-L31):
    Removed the Auto-Document workflow.
    *
    [`.github/workflows/Linter.yml`](diffhunk://#diff-482e65806ed9e4a7320f14964764086b91fed4a28d12e4efde1776472e147e79R30-R33):
    Disabled validation for markdown, JSON, YAML, and JSCPD in the linter
    configuration.
    
    ### New helper functions:
    *
    [`scripts/helpers/Build-PSModuleDocumentation.ps1`](diffhunk://#diff-95b236851bd9052577a2cdf569f08e195e7d6cc424f6cb6d074ccc10b2cb7241R1-R126):
    Added a new function to build module documentation, including generating
    markdown help and fixing markdown code blocks.
    *
    [`scripts/helpers/Import-PSModule.ps1`](diffhunk://#diff-e4bfa9c4a169541b0894ed0c4f60df9fcde101f9bd11ea736e8c8173b15de476R1-R50):
    Added a new function to import a PowerShell module from a specified
    path.
    *
    [`scripts/helpers/Resolve-PSModuleDependency.ps1`](diffhunk://#diff-48557b471e7d62a89be3627235334ab8a39eb6119174abb61714b92d844508e2R1-R64):
    Added a new function to resolve module dependencies based on the
    manifest file.
    
    ### Updates to action configuration:
    *
    [`action.yml`](diffhunk://#diff-1243c5424efaaa19bd8e813c5e6f6da46316e63761421b3e5f5c8ced9a36e6b6L1-R79):
    Updated the action name, description, and inputs to better reflect the
    new functionality. Added steps to download module artifacts, run the
    documentation build script, and upload documentation artifacts.
    
    ### Documentation and configuration updates:
    *
    [`tests/outputs/modules/PSModuleTest/PSModuleTest.psd1`](diffhunk://#diff-39b31e19473e744165c2dd765b29282450842905b1e7c37f54f356a7d87535a6R1-R75):
    Added a new module manifest file for `PSModuleTest` with detailed module
    configuration.
    *
    [`.gitattributes`](diffhunk://#diff-618cd5b83d62060ba3d027e314a21ceaf75d36067ff820db126642944145393eR1-R15):
    Added configuration to handle different file types and ensure proper
    line endings.
    
    These changes collectively improve the automation, documentation, and
    configuration management of the project.
    
    ## Type of change
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [ ] 📖 [Docs]
    - [ ] 🪲 [Fix]
    - [x] 🩹 [Patch]
    - [ ] ⚠️ [Security fix]
    - [ ] 🚀 [Feature]
    - [ ] 🌟 [Breaking change]
    
    ## Checklist
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas

---
+++ scripts/helpers/Build-PSModuleDocumentation.ps1
@@ -0,0 +1,126 @@
+﻿#Requires -Modules @{ ModuleName = 'GitHub'; ModuleVersion = '0.13.2' }
+#Requires -Modules @{ ModuleName = 'Utilities'; ModuleVersion = '0.3.0' }
+
+function Build-PSModuleDocumentation {
+    <#
+        .SYNOPSIS
+        Builds a module.
+
+        .DESCRIPTION
+        Builds a module.
+    #>
+    [CmdletBinding()]
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute(
+        'PSReviewUnusedParameter', '', Scope = 'Function',
+        Justification = 'LogGroup - Scoping affects the variables line of sight.'
+    )]
+    param(
+        # Name of the module.
+        [Parameter(Mandatory)]
+        [string] $ModuleName,
+
+        # Path to the folder where the modules are located.
+        [Parameter(Mandatory)]
+        [string] $ModuleSourceFolderPath,
+
+        # Path to the folder where the built modules are outputted.
+        [Parameter(Mandatory)]
+        [string] $ModulesOutputFolderPath,
+
+        # Path to the folder where the documentation is outputted.
+        [Parameter(Mandatory)]
+        [string] $DocsOutputFolderPath
+    )
+
+    LogGroup "Documenting module [$ModuleName]" {
+        Write-Host "Source path:          [$ModuleSourceFolderPath]"
+        if (-not (Test-Path -Path $ModuleSourceFolderPath)) {
+            Write-Error "Source folder not found at [$ModuleSourceFolderPath]"
+            exit 1
+        }
+        $moduleSourceFolder = Get-Item -Path $ModuleSourceFolderPath
+        Write-Host "Module source folder: [$moduleSourceFolder]"
+
+        $moduleOutputFolder = New-Item -Path $ModulesOutputFolderPath -Name $ModuleName -ItemType Directory -Force
+        Write-Host "Module output folder: [$moduleOutputFolder]"
+
+        $docsOutputFolder = New-Item -Path $DocsOutputFolderPath -ItemType Directory -Force
+        Write-Host "Docs output folder:   [$docsOutputFolder]"
+    }
+
+    LogGroup 'Build docs - Generate markdown help' {
+        Add-PSModulePath -Path (Split-Path -Path $ModuleOutputFolder -Parent)
+        $ModuleName | Remove-Module -Force -ErrorAction SilentlyContinue
+        Import-PSModule -Path $ModuleOutputFolder -ModuleName $ModuleName
+        Write-Host ($ModuleName | Get-Module)
+        $null = New-MarkdownHelp -Module $ModuleName -OutputFolder $DocsOutputFolder -Force -Verbose
+    }
+
+    LogGroup 'Build docs - Fix markdown code blocks' {
+        Get-ChildItem -Path $DocsOutputFolder -Recurse -Force -Include '*.md' | ForEach-Object {
+            $content = Get-Content -Path $_.FullName
+            $fixedOpening = $false
+            $newContent = @()
+            foreach ($line in $content) {
+                if ($line -match '^```$' -and -not $fixedOpening) {
+                    $line = $line -replace '^```$', '```powershell'
+                    $fixedOpening = $true
+                } elseif ($line -match '^```.+$') {
+                    $fixedOpening = $true
+                } elseif ($line -match '^```$') {
+                    $fixedOpening = $false
+                }
+                $newContent += $line
+            }
+            $newContent | Set-Content -Path $_.FullName
+        }
+    }
+
+    LogGroup 'Build docs - Fix markdown escape characters' {
+        Get-ChildItem -Path $DocsOutputFolder -Recurse -Force -Include '*.md' | ForEach-Object {
+            $content = Get-Content -Path $_.FullName -Raw
+            $content = $content -replace '\\`', '`'
+            $content = $content -replace '\\\[', '['
+            $content = $content -replace '\\\]', ']'
+            $content = $content -replace '\\\<', '<'
+            $content = $content -replace '\\\>', '>'
+            $content = $content -replace '\\\\', '\'
+            $content | Set-Content -Path $_.FullName
+        }
+    }
+
+    LogGroup 'Build docs - Structure markdown files to match source files' {
+        $PublicFunctionsFolder = Join-Path $ModuleSourceFolder.FullName 'functions\public' | Get-Item
+        Get-ChildItem -Path $DocsOutputFolder -Recurse -Force -Include '*.md' | ForEach-Object {
+            $file = $_
+            Write-Host "Processing:        $file"
+
+            # find the source code file that matches the markdown file
+            $scriptPath = Get-ChildItem -Path $PublicFunctionsFolder -Recurse -Force | Where-Object { $_.Name -eq ($file.BaseName + '.ps1') }
+            Write-Host "Found script path: $scriptPath"
+            $docsFilePath = ($scriptPath.FullName).Replace($PublicFunctionsFolder.FullName, $DocsOutputFolder.FullName).Replace('.ps1', '.md')
+            Write-Host "Doc file path:     $docsFilePath"
+            $docsFolderPath = Split-Path -Path $docsFilePath -Parent
+            New-Item -Path $docsFolderPath -ItemType Directory -Force
+            Move-Item -Path $file.FullName -Destination $docsFilePath -Force
+        }
+        # Get the MD files that are in the public functions folder and move them to the same place in the docs folder
+        Get-ChildItem -Path $PublicFunctionsFolder -Recurse -Force -Include '*.md' | ForEach-Object {
+            $file = $_
+            Write-Host "Processing:        $file"
+            $docsFilePath = ($file.FullName).Replace($PublicFunctionsFolder.FullName, $DocsOutputFolder.FullName)
+            Write-Host "Doc file path:     $docsFilePath"
+            $docsFolderPath = Split-Path -Path $docsFilePath -Parent
+            New-Item -Path $docsFolderPath -ItemType Directory -Force
+            Move-Item -Path $file.FullName -Destination $docsFilePath -Force
+        }
+    }
+
+    Get-ChildItem -Path $DocsOutputFolder -Recurse -Force -Include '*.md' | ForEach-Object {
+        $fileName = $_.Name
+        $hash = (Get-FileHash -Path $_.FullName -Algorithm SHA256).Hash
+        LogGroup " - [$fileName] - [$hash]" {
+            Show-FileContent -Path $_
+        }
+    }
+}
--- scripts/main.ps1
+++ scripts/main.ps1
@@ -1,24 +1,53 @@
-#Requires -Modules GitHub
+﻿#Requires -Modules Utilities
 
 [CmdletBinding()]
-param(
-    [Parameter()]
-    [string] $Subject = $env:GITHUB_ACTION_INPUT_subject
-)
+param()
 
-begin {
-    $scriptName = $MyInvocation.MyCommand.Name
-    Write-Debug "[$scriptName] - Start"
-}
-
-process {
-    try {
-        Write-Output "Hello, $Subject!"
-    } catch {
-        throw $_
+$path = (Join-Path -Path $PSScriptRoot -ChildPath 'helpers') | Get-Item | Resolve-Path -Relative
+LogGroup "Loading helper scripts from [$path]" {
+    Get-ChildItem -Path $path -Filter '*.ps1' -Recurse | Resolve-Path -Relative | ForEach-Object {
+        Write-Host "$_"
+        . $_
     }
 }
 
-end {
-    Write-Debug "[$scriptName] - End"
+LogGroup 'Loading inputs' {
+    $moduleName = ($env:GITHUB_ACTION_INPUT_Name | IsNullOrEmpty) ? $env:GITHUB_REPOSITORY_NAME : $env:GITHUB_ACTION_INPUT_Name
+    Write-Host "Module name:         [$moduleName]"
+
+    $moduleSourceFolderPath = Join-Path -Path $env:GITHUB_WORKSPACE -ChildPath $env:GITHUB_ACTION_INPUT_Path/$moduleName
+    if (-not (Test-Path -Path $moduleSourceFolderPath)) {
+        $moduleSourceFolderPath = Join-Path -Path $env:GITHUB_WORKSPACE -ChildPath $env:GITHUB_ACTION_INPUT_Path
+    }
+    Write-Host "Source module path:  [$moduleSourceFolderPath]"
+    if (-not (Test-Path -Path $moduleSourceFolderPath)) {
+        throw "Module path [$moduleSourceFolderPath] does not exist."
+    }
+
+    $modulesOutputFolderPath = Join-Path $env:GITHUB_WORKSPACE $env:GITHUB_ACTION_INPUT_ModulesOutputPath
+    Write-Host "Modules output path: [$modulesOutputFolderPath]"
+    $docsOutputFolderPath = Join-Path $env:GITHUB_WORKSPACE $env:GITHUB_ACTION_INPUT_DocsOutputPath
+    Write-Host "Docs output path:    [$docsOutputFolderPath]"
 }
+
+LogGroup 'Build local scripts' {
+    Write-Host 'Execution order:'
+    $scripts = Get-ChildItem -Filter '*build.ps1' -Recurse | Sort-Object -Property Name | Resolve-Path -Relative
+    $scripts | ForEach-Object {
+        Write-Host " - $_"
+    }
+    $scripts | ForEach-Object {
+        LogGroup "Build local scripts - [$_]" {
+            . $_
+        }
+    }
+}
+
+$params = @{
+    ModuleName              = $moduleName
+    ModuleSourceFolderPath  = $moduleSourceFolderPath
+    ModulesOutputFolderPath = $modulesOutputFolderPath
+    DocsOutputFolderPath    = $docsOutputFolderPath
+}
+
+Build-PSModuleDocumentation @params
