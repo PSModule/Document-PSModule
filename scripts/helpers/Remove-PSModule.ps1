71cb0720ec9b0d8493c8336ba9c7458250721b65
Marius Storhaug <marstor@hotmail.com>
2025-02-24 13:40:18 +0100

    Enhance Remove-PSModule.ps1 to improve module and command removal process with detailed logging and accurate uninstallation

--- scripts/helpers/Remove-PSModule.ps1
+++ scripts/helpers/Remove-PSModule.ps1
@@ -21,8 +21,25 @@
     )
 
     if ($PSCmdlet.ShouldProcess('Target', "Remove module [$Name]")) {
-        Write-Host "Removing module [$Name]"
-        Get-Module -Name $Name -ListAvailable | Remove-Module -Force
-        Get-PSResource -Name $Name | Uninstall-PSResource -SkipDependencyCheck
+        Write-Host "[$Name] - Remove module"
+        $importedModule = Get-Module -ListAvailable | Where-Object { $_.Name -eq $Name }
+        Write-Host " - Found [$($importedModule.Count)] modules to remove"
+        foreach ($module in $importedModule) {
+            Write-Host " - Removing module [$($module.Name)]"
+            $module | Remove-Module -Force
+        }
+        $commands = Get-ChildItem -Path Function: | Where-Object { $_.Source -eq $Name }
+        Write-Host " - Found [$($commands.Count)] commands to remove"
+        foreach ($command in $commands) {
+            Write-Host " - Removing command [$($command.Name)]"
+            $command | Remove-Item -Force
+        }
+        $installedModule = Get-InstalledPSResource | Where-Object { $_.Name -eq $Name }
+        Write-Host " - Found [$($installedModule.Count)] installed modules to remove"
+        foreach ($module in $installedModule) {
+            Write-Host " - Uninstalling module [$($module.Name)]"
+            $module | Uninstall-PSResource -SkipDependencyCheck
+        }
+        Get-Command -Module $Name | Format-Table -AutoSize
     }
 }
