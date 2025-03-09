25b8d499f8d9163be4581f4128de72e07d8f3eee
Marius Storhaug <marstor@hotmail.com>
2025-03-09 16:15:01 +0100

    Enhance module installation process with retry logic and improved error handling

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -8,8 +8,24 @@ param()
 $PSStyle.OutputRendering = 'Ansi'
 
 'platyPS' | ForEach-Object {
-    Install-PSResource -Name $_ -WarningAction SilentlyContinue -TrustRepository -Repository PSGallery
-    Import-Module -Name $_
+    $name = $_
+    Write-Output "Installing module: $name"
+    $retryCount = 5
+    $retryDelay = 10
+    for ($i = 0; $i -lt $retryCount; $i++) {
+        try {
+            Install-PSResource -Name $name -WarningAction SilentlyContinue -TrustRepository -Repository PSGallery
+            break
+        } catch {
+            Write-Warning "Installation of $($psResourceParams.Name) failed with error: $_"
+            if ($i -eq $retryCount - 1) {
+                throw
+            }
+            Write-Warning "Retrying in $retryDelay seconds..."
+            Start-Sleep -Seconds $retryDelay
+        }
+    }
+    Import-Module -Name $name
 }
 
 $path = (Join-Path -Path $PSScriptRoot -ChildPath 'helpers') | Get-Item | Resolve-Path -Relative
